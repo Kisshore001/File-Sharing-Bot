b014ac09dce47ae52e3723906b1f7008ba5f7900
mrsasuke07 <kuraliniyan232@gmail.com>
2025-03-01 19:06:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>👨‍💻 Developer : <a href='https://t.me/Satorux_Gojo'>Satoru Gojo</a>\n📑 Language : <a href='https://python.org/'>Python3</a>\n📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n🌐 Server : <a href='https://heroku.com/'>Heroku</a>\n📣 Channel : <a href='https://t.me/+DAa8VkOygMdhZWRl'>Anime in Tamil</a>\n🏅Owner : <a href='https://t.me/Mrsaskuke07'>Mr Sasuke</a></b>",
+            text = f"<b>👨‍💻 Developer : <a href='https://t.me/Satorux_Gojo'>Satoru Gojo</a>\n📑 Language : <a href='https://python.org/'>Python3</a>\n📚 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n🌐 Server : <a href='https://heroku.com/'>Heroku</a>\n📣 Channel : <a href='https://t.me/+DAa8VkOygMdhZWRl'>Anime in Tamil</a>\n🏅Owner : <a href='https://t.me/Mrsasuke07'>Mr Sasuke</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
